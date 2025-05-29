9758bec62a831ed838635d3fe12eeac1ec4dc0a2
Abakash-repo <abakash.ghadia@rapyder.com>
2025-05-29 17:59:55 +0530

    Update pipeline.py

--- pipelines/abalone/pipeline.py
+++ pipelines/abalone/pipeline.py
@@ -159,7 +159,7 @@ def get_pipeline(
 
     # Processing step for feature engineering
     sklearn_processor = SKLearnProcessor(
-        framework_version="1.2-1",
+        framework_version="0.23-1",
         instance_type=processing_instance_type,
         instance_count=processing_instance_count,
         base_job_name=f"{base_job_prefix}/sklearn-movie-preprocess",
